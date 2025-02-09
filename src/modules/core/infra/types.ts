7b35242fb367017d2a92f6fafe6fe3a870060c58
Robson Maia <robsonmvieira@gmail.com>
2025-02-09 20:44:28 +0000

    :sparkles: create dictionary key to use with dependency injection

---
+++ src/modules/core/infra/types.ts
@@ -0,0 +1,9 @@
+const Types = {
+	ICategoryRepository: Symbol.for("ICategoryRepository"),
+	IGatewayContract: Symbol.for("IGatewayContract"),
+	CreateCategoryUseCase: Symbol.for("CreateCategoryUseCase"),
+	UpdateCategoryUseCase: Symbol.for("UpdateCategoryUseCase"),
+	DeleteCategoryUseCase: Symbol.for("DeleteCategoryUseCase"),
+	ListCategoryUseCase: Symbol.for("ListCategoryUseCase")
+}
+export { Types }
