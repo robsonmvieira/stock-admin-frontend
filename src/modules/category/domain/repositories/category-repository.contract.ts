8dd9424ad7ab698668ddfedd1fbc8cead789d6e3
Robson Maia <robsonmvieira@gmail.com>
2025-02-09 17:03:52 +0000

    :necktie: category interface and concrete class

---
+++ src/modules/category/domain/repositories/category-repository.contract.ts
@@ -0,0 +1,10 @@
+import type { CategoryEntity } from "@modules/category/domain/entities"
+import type { BaseResponse } from "@modules/core/domain/entities"
+
+export interface ICategoryRepository {
+	save(category: CategoryEntity): Promise<void>
+	update(category: CategoryEntity): Promise<void>
+	delete(categoryId: string): Promise<void>
+	find(categoryId: string): Promise<BaseResponse<CategoryEntity>>
+	list(): Promise<BaseResponse<CategoryEntity[]>>
+}
---
+++ src/modules/category/infra/repositories/category.repository.ts
@@ -0,0 +1,28 @@
+import type { ICategoryRepository } from "@modules/category/domain/repositories"
+import type { BaseResponse } from "@modules/core/domain/entities"
+import type { IGatewayContract } from "@modules/core/domain/repositories/gateway.contract"
+import { Types } from "@modules/core/infra/types.ts"
+import { inject, injectable } from "inversify"
+import type { CategoryEntity } from "../../domain/entities"
+
+@injectable()
+export class CategoryRepository implements ICategoryRepository {
+	@inject(Types.IGatewayContract)
+	private repo: IGatewayContract
+
+	async update(category: CategoryEntity): Promise<void> {
+		await this.repo.put(`categories/${category.id}`, category)
+	}
+	async delete(category: string): Promise<void> {
+		await this.repo.delete(`categories/${category}`)
+	}
+	async find(categoryId: string): Promise<BaseResponse<CategoryEntity>> {
+		return await this.repo.get(`categories/${categoryId}`)
+	}
+	async list(): Promise<BaseResponse<CategoryEntity[]>> {
+		return await this.repo.get("categories")
+	}
+	async save(category: CategoryEntity): Promise<void> {
+		await this.repo.post("categories", category)
+	}
+}
