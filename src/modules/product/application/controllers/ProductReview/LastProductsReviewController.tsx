75fb18ebd7d8432ceace9ee9b8bdaa2e6326ccac
Robson Maia <robsonmvieira@gmail.com>
2025-02-10 07:21:18 +0000

    :sparkles: add empty LastProductsReviewController component

---
+++ src/modules/product/application/controllers/ProductReview/LastProductsReviewController.tsx
@@ -0,0 +1,9 @@
+export function LastProductsReviewController() {
+	return (
+		<div>
+			<h1>Last Products Review</h1>
+		</div>
+	)
+}
+
+LastProductsReviewController.DisplayName = "LastProductsReview"
